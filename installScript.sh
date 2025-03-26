f700e986a2bb7ace154db2521bfd02ed00d899c6
mango7006 <magnus.van.der.wal@outlook.com>
2025-03-26 18:44:55 +0100

    updated install script Mar/26/18:44

--- installScript.sh
+++ installScript.sh
@@ -34,7 +34,14 @@ fi
 read -r -p "Do you want to install dependencies and packages? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   echo "Downloading packages"
-  sudo pacman --needed -S "$(<pkgs.txt)"
+  sudo pacman -S --needed --noconfirm neovim
+  sudo pacman -S --needed --noconfirm starship zoxide bat eza ripgrep yazi arp-scan
+  sudo pacman -S --needed --noconfirm lazygit luarocks npm typescript unzip minizip fzf
+  sudo pacman -S --needed --noconfirm ttf-droid ttf-font-awesome ttf-ibm-plex ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
+  sudo pacman -S --needed --noconfirm bitwarden firefox obsidian signal-desktop libnotify spotify-launcher obs-studio feh evince thunar mpv vlc nwg-look
+  sudo pacman -S --needed --noconfirm brightnessctl gammastep
+  sudo pacman -S --needed --noconfirm dunst hyprland hyprlock hyprpicker qt5-wayland qt6-5compat qt6-shadertools qt6-wayland swww waybar wofi xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xdg-desktop-portal-wlr kitty ly wl-clipboard
+  systemctl enable NetworkManager sshd ufw bluetooth ly systemd-timesyncd
 else
   echo -e "Not installing packages. \n WARNING: this might make my dotfiles unuseable."
 fi
