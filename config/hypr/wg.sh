0d40530f8cceefe65738b7b080b03c0e29d8123a
mango7006 <magnus.van.der.wal@outlook.com>
2025-04-14 22:22:34 +0200

    ... Apr/14/22:22

--- config/hypr/wg.sh
+++ config/hypr/wg.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+PASSWORD=$(zenity --password --title="Wireguard Toggle")
 INTERFACE="wg0"
 
 if ip link show | grep -q "$INTERFACE"; then
   notify-send "🔌 Disconnecting VPN ($INTERFACE)"
-  kitty sudo wg-quick down "$INTERFACE"
+  echo $PASSWORD | sudo -S wg-quick down "$INTERFACE"
 else
   notify-send "🔒 Connecting VPN ($INTERFACE)"
-  kitty sudo wg-quick up "$INTERFACE"
+  echo $PASSWORD | sudo -S wg-quick up "$INTERFACE"
 fi
